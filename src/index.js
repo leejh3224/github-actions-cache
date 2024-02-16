357b9a52ac2db93f477f58540c93bfde2f12e48b
teddy <junhyung3224@gmail.com>
2024-02-16 23:05:57 +0900

    change code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/hello', (req, res) => {
-    res.json({ hello: 'world!' });
+    res.json({ hello: 'world!!' });
 });
 
 app.listen(port, () => {
